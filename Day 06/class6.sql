be3cdd2ab61d1cde812efcb0e4b288f925bf2f42
TalishRaza78 <talish.s78@gmail.com>
2025-06-27 19:35:52 +0530

    HAVING CLAUSE AND GENERAL ORDER

--- Day 06/class6.sql
+++ Day 06/class6.sql
@@ -3,6 +3,23 @@
 
 SELECT city , COUNT(name) FROM student GROUP BY city HAVING MAX(marks) > 90 ;
 
+--  question 
+orders
+
+| order\_id | customer\_id | amount |
+| --------- | ------------ | ------ |
+| 1         | 101          | 250    |
+| 2         | 102          | 450    |
+| 3         | 101          | 300    |
+| 4         | 103          | 150    |
+| 5         | 102          | 200    |
+
+
+SELECT customer_id, SUM(amount) AS total_amount
+FROM orders
+GROUP BY customer_id
+HAVING SUM(amount) > 500;
+
 --GENERAL ORDER 
 SELECT column(s)
 FROM table_name 
